0e3128daef3cd0db903d05ec7d6c9d0fa789da83
ComputerNerd <mailboxer123@yahoo.com>
2014-06-23 21:31:42 -0500

    Update main.c
    Added the option to specify variable name.

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-//compile gcc main.c -Wall -Wextra -O2 -o bin2h
+//compile gcc main.c -Wall -Wextra -O2 -s -o bin2h
 int main(int argc,char ** argv){
-	if(argc!=3){
-		puts("Usage: in out");
+	if((argc!=3)&&(argc!=4)){
+		puts("Usage: in out [array name]");
 		return 1;
 	}
 	FILE*fi,*fo;
@@ -14,7 +14,10 @@ int main(int argc,char ** argv){
 	uint32_t sizeh=ftell(fi);
 	rewind(fi);
 	uint32_t sizel;
-	fprintf(fo,"const uint8_t %s[]={\n",argv[1]);
+	if(argc==4)
+		fprintf(fo,"static const uint8_t %s[]={\n",argv[3]);
+	else
+		fprintf(fo,"static const uint8_t %s[]={\n",argv[1]);
 	for(sizel=0;sizel<sizeh-1;++sizel){
 		fprintf(fo,"%d,",fgetc(fi));
 		if((sizel&127)==127)
